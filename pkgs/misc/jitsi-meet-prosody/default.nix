5feba4cb5e3d6c0efb30c47479631ba9245728e0
Marcel Novotny <marceln@xcroco.com>
2021-12-11 02:49:01 +0100

    updated version

--- pkgs/misc/jitsi-meet-prosody/default.nix
+++ pkgs/misc/jitsi-meet-prosody/default.nix
@@ -2,7 +2,7 @@
 
 stdenv.mkDerivation rec {
   pname = "jitsi-meet-prosody";
-  version = "1.0.5415";
+  version = "1.0.5675";
   src = fetchurl {
     url = "https://download.jitsi.org/stable/${pname}_${version}-1_all.deb";
     sha256 = "XvW+bAuad1IKJPZzVJBkT7vA2BBDFQBvTWtbyK/in6A=";
